411a5c6f56a17c6035d59e952fd62e2e1a280acf
Richard Benkovsky <richard.benkovsky@gmail.com>
2017-01-25 14:04:20 +0100

    FEATURE: CFAL-5 Add requestId processing filter

--- cfal-restapi/src/main/java/com/gooddata/cfal/restapi/config/WebConfig.java
+++ cfal-restapi/src/main/java/com/gooddata/cfal/restapi/config/WebConfig.java
@@ -4,6 +4,7 @@
 package com.gooddata.cfal.restapi.config;
 
 import com.gooddata.cfal.restapi.exception.AuditlogExceptionTranslatorAdvice;
+import com.gooddata.commons.web.filter.LoggingContextSetupFilter;
 import com.gooddata.context.GdcCallContextFilter;
 import com.gooddata.exception.servlet.HttpExceptionTranslator;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -21,16 +22,24 @@ public class WebConfig {
     @Bean
     public FilterRegistrationBean gdcCallContextFilterRegistration() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(gdcCallContextFilter());
+        registration.setFilter(new GdcCallContextFilter());
         registration.addUrlPatterns(DOMAIN_AUDIT_URI + "/*");
         registration.setName("GDC_FILTER");
         registration.setOrder(1);
         return registration;
     }
 
+    /**
+     * register LoggingContextSetupFilter as Filter
+     */
     @Bean
-    public GdcCallContextFilter gdcCallContextFilter() {
-        return new GdcCallContextFilter();
+    public FilterRegistrationBean gdcRequestIdHandlingFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new LoggingContextSetupFilter());
+        registration.addUrlPatterns(DOMAIN_AUDIT_URI + "/*");
+        registration.setName("GDC_LOGGING_CONTEXT_FILTER");
+        registration.setOrder(2);
+        return registration;
     }
 
     /**
