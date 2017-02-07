52bf02d38e2238d06e0d08e062e13bd2366ab704
Richard Benkovsky <richard.benkovsky@gmail.com>
2017-02-07 10:44:43 +0100

    TRIVIAL: Remove hashCode and equals from DTOs
    Both equals and hashCode methods are not used in production code but
    they are rather there only for testing purposes. So sole purpose of this
    code is simplification of the production code and some cleanup in the
    testing code.

--- cfal-restapi/src/main/java/com/gooddata/cfal/restapi/dto/AuditEventsDTO.java
+++ cfal-restapi/src/main/java/com/gooddata/cfal/restapi/dto/AuditEventsDTO.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.gooddata.collections.PageableList;
 import com.gooddata.collections.Paging;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -31,45 +30,4 @@ public class AuditEventsDTO extends PageableList<AuditEventDTO> {
         super(items, paging, links);
     }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-
-        final AuditEventsDTO that = (AuditEventsDTO) o;
-
-        if (!Arrays.deepEquals(this.toArray(), that.toArray())) {
-            return false;
-        }
-
-        if ((this.getPaging() == null && that.getPaging() != null) || (this.getPaging() != null && that.getPaging() == null)) {
-            return false;
-        }
-
-        if(this.getPaging() != null && that.getPaging() != null) {
-            if ((this.getPaging().getNextUri() == null && that.getPaging().getNextUri() != null) ||  (this.getPaging().getNextUri() != null && that.getPaging().getNextUri() == null)) {
-                return false;
-            }
-            if(this.getPaging().getNextUri() != that.getPaging().getNextUri()) {
-                if (!this.getPaging().getNextUri().equals(that.getPaging().getNextUri())) {
-                    return false;
-                }
-            }
-        }
-
-        if(this.getLinks() != null && that.getLinks() != null) {
-            return this.getLinks().equals(that.getLinks());
-        }
-
-        return this.getLinks() == that.getLinks();
-    }
-
-    @Override
-    public int hashCode() {
-        return iterator().hashCode() +
-                (getNextPage() == null ? 0 : getNextPage().hashCode()) +
-                (getLinks() == null ? 0 : getLinks().hashCode());
-    }
 }
--- cfal-restapi/src/test/java/com/gooddata/cfal/restapi/dto/AuditEventsDTOTest.java
+++ cfal-restapi/src/test/java/com/gooddata/cfal/restapi/dto/AuditEventsDTOTest.java
@@ -18,23 +18,29 @@ import java.util.HashMap;
 
 import static com.gooddata.cfal.restapi.dto.AuditEventDTO.ADMIN_URI;
 import static com.gooddata.cfal.restapi.util.DateUtils.date;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 
 @RunWith(SpringRunner.class)
 @JsonTest
 public class AuditEventsDTOTest {
 
+    private static final AuditEventDTO EVENT_1 = new AuditEventDTO("123", "default", "user123", date("1993-03-09"), date("1993-03-09"));
+    private static final AuditEventDTO EVENT_2 = new AuditEventDTO("456", "default", "user456", date("1993-03-09"), date("1993-03-09"));
+    private static final String NEXT_URI = "/gdc/audit/admin/events?offset=456&limit=100";
     @Autowired
     private JacksonTester<AuditEventsDTO> json;
 
-    private final AuditEventsDTO events = new AuditEventsDTO(
-            Arrays.asList(new AuditEventDTO("123", "default", "user123", date("1993-03-09"), date("1993-03-09")),
-                    new AuditEventDTO("456", "default", "user456", date("1993-03-09"), date("1993-03-09"))),
-            new Paging("/gdc/audit/admin/events?offset=456&limit=100"),
+    private static final AuditEventsDTO EVENTS = new AuditEventsDTO(
+            Arrays.asList(EVENT_1, EVENT_2),
+            new Paging(NEXT_URI),
             new HashMap<String, String>() {{
                 put("self", ADMIN_URI);
             }});
 
-    private final AuditEventsDTO emptyEvents = new AuditEventsDTO(
+    private static final AuditEventsDTO EMPTY_EVENTS = new AuditEventsDTO(
             Collections.emptyList(),
             new Paging(null),
             new HashMap<String, String>() {{
@@ -43,24 +49,30 @@ public class AuditEventsDTOTest {
 
     @Test
     public void testSerialize() throws Exception {
-        json.write(events).assertThat().isEqualToJson("auditEvents.json");
+        json.write(EVENTS).assertThat().isEqualToJson("auditEvents.json");
     }
 
     @Test
     public void testDeserialize() throws Exception {
         String content = IOUtils.toString(getClass().getResourceAsStream("auditEvents.json"));
 
-        json.parse(content).assertThat().isEqualTo(events);
+        final AuditEventsDTO deserialized = json.parse(content).getObject();
+        assertThat(deserialized.getPaging().getNextUri(), is(NEXT_URI));
+        assertThat(deserialized, hasSize(2));
+        assertThat(deserialized.get(0).getId(), is(EVENT_1.getId()));
+        assertThat(deserialized.get(1).getId(), is(EVENT_2.getId()));
     }
 
     @Test
-    public void testSerializeEmptyEvents() throws Exception{
-        json.write(emptyEvents).assertThat().isEqualToJson("emptyAuditEvents.json");
+    public void testSerializeEmptyEvents() throws Exception {
+        json.write(EMPTY_EVENTS).assertThat().isEqualToJson("emptyAuditEvents.json");
     }
 
     @Test
     public void testDeserializeEmptyEvents() throws Exception {
         String content = IOUtils.toString(getClass().getResourceAsStream("emptyAuditEvents.json"));
-        json.parse(content).assertThat().isEqualTo(emptyEvents);
+        final AuditEventsDTO deserialized = json.parse(content).getObject();
+        assertThat(deserialized.getPaging().getNextUri(), nullValue());
+        assertThat(deserialized, hasSize(0));
     }
 }
