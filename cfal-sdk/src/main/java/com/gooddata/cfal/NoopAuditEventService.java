821b318e5d820e968b818e056a2b4fe9e239c6da
Martin Caslavsky <mcaslavsky@gmail.com>
2017-04-07 14:02:09 +0200

    CFAL-123: add structured log to NoopAuditEventService
    This allows components to verify logger in tests

--- cfal-sdk/src/main/java/com/gooddata/cfal/NoopAuditEventService.java
+++ cfal-sdk/src/main/java/com/gooddata/cfal/NoopAuditEventService.java
@@ -8,23 +8,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.gooddata.cfal.AuditLogEventWriterBase.format;
+import static org.apache.commons.lang3.Validate.notNull;
 
 /**
  * Audit log service which logs messages using SLF4J. Suitable for testing only.
  */
 public class NoopAuditEventService extends AbstractAuditLogService {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger;
 
     public NoopAuditEventService() {
+        this(LoggerFactory.getLogger(NoopAuditEventService.class));
+    }
+
+    NoopAuditEventService(final Logger logger) {
         super("test");
+        this.logger = notNull(logger, "logger");
     }
 
     @Override
     protected void doLogEvent(final AuditLogEvent event) {
         try {
             final String eventData = format(event);
-            logger.info(eventData);
+            logger.info("component={} type={} userLogin={} userIp={} domainId={} success={}", event.getComponent(),
+                    event.getType(), event.getUserLogin(), event.getUserIp(), event.getDomainId(), event.isSuccess());
+            logger.debug(eventData);
         } catch (JsonProcessingException e) {
             logger.error("Unable to write event={}", event.getType(), e);
         }
