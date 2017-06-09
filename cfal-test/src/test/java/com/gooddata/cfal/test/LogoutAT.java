8cc25b05cf8b585518b837220d897a015243f2ea
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2017-06-09 11:25:17 +0200

    FEATURE: CFAL-91 add logout message AT

---
+++ cfal-test/src/test/java/com/gooddata/cfal/test/LogoutAT.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2007-2017, GoodData(R) Corporation. All rights reserved.
+ */
+package com.gooddata.cfal.test;
+
+import com.gooddata.cfal.restapi.dto.AuditEventDTO;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class LogoutAT extends AbstractAT {
+
+    private static final String MESSAGE_TYPE = "STANDARD_LOGOUT";
+
+    @BeforeClass
+    public void setUp() {
+        gd.getAccountService().logout();
+    }
+
+    @Test(groups = MESSAGE_TYPE)
+    public void testLogoutMessageUserApi() throws InterruptedException {
+        doTestUserApi(pageCheckPredicate());
+    }
+
+    @Test(groups = MESSAGE_TYPE)
+    public void testLogoutMessageAdminApi() throws InterruptedException {
+        doTestAdminApi(pageCheckPredicate());
+    }
+
+    private Predicate<List<AuditEventDTO>> pageCheckPredicate() {
+        return (auditEvents) -> auditEvents.stream().anyMatch(e -> e.getUserLogin().equals(account.getLogin()) && e.getType().equals(MESSAGE_TYPE));
+    }
+
+}
