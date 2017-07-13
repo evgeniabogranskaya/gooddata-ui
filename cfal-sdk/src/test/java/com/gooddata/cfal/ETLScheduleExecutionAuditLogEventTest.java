8841feb076fb62f99e63ad572448251dcb09b951
Martin Caslavsky <mcaslavsky@gmail.com>
2017-07-13 09:50:05 +0200

    remove copy&pasted code

--- cfal-sdk/src/test/java/com/gooddata/cfal/ETLScheduleExecutionAuditLogEventTest.java
+++ cfal-sdk/src/test/java/com/gooddata/cfal/ETLScheduleExecutionAuditLogEventTest.java
@@ -3,41 +3,17 @@
  */
 package com.gooddata.cfal;
 
+import org.junit.Test;
 
 import static com.gooddata.cfal.ETLDataloadProcessExecutionAuditLogEvent.ETL_SCHEDULE_CHANGE;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import com.gooddata.context.ThreadContextUtils;
-import com.gooddata.context.TransferableGdcCallContext;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+public class ETLScheduleExecutionAuditLogEventTest extends AbstractProjectAuditLogEventTest {
 
-public class ETLScheduleExecutionAuditLogEventTest {
-
-    private static final String USER_LOGIN = "bear@gooddata.com";
-    private static final String USER_IP = "127.0.0.1";
-    private static final String DOMAIN_ID = "default";
-    private static final String PROJECT_ID = "FoodMartDemo";
     private static final String PROCESS = "/gdc/projects/" + PROJECT_ID + "/dataload/processes/TestProcessId";
     private static final String SCHEDULE = "/gdc/projects/" + PROJECT_ID + "/schedules/TestScheduleId";
     private static final String EXECUTION = SCHEDULE + "/executions/ExecutionId";
-
-    @Before
-    public void setUp() {
-        final TransferableGdcCallContext context = new TransferableGdcCallContext();
-        context.setUserLogin(USER_LOGIN);
-        context.setClientIp(USER_IP);
-        context.setDomainId(DOMAIN_ID);
-        context.setProjectId(PROJECT_ID);
-        ThreadContextUtils.setUpGdcCallContext(context);
-    }
-
-    @After
-    public void tearDown() {
-        ThreadContextUtils.clearGdcCallContext();
-    }
 
     @Test
     public void shouldSetValuesFromGdcCallContext() {
