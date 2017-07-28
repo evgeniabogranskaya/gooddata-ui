00513bc765ee0a1226c6e99f4cf817d14763b600
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2017-07-28 13:09:39 +0200

    FEATURE: CFAL-54 monitor background creation of indexes

--- cfal-restapi/src/test/java/com/gooddata/cfal/restapi/task/CreateIndexTaskTest.java
+++ cfal-restapi/src/test/java/com/gooddata/cfal/restapi/task/CreateIndexTaskTest.java
@@ -3,30 +3,58 @@
  */
 package com.gooddata.cfal.restapi.task;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import com.gooddata.cfal.restapi.repository.AuditLogEventRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static com.gooddata.cfal.restapi.task.CreateIndexTask.LOGIN_INDEX_TIMER_NAME;
+import static com.gooddata.cfal.restapi.task.CreateIndexTask.TTL_INDEX_TIMER_NAME;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+
+import java.util.concurrent.TimeUnit;
 
 public class CreateIndexTaskTest {
 
     @Mock
+    private Timer ttlTimer;
+    @Mock
+    private Timer loginTimer;
+    @Mock
     private AuditLogEventRepository repository;
+    @Mock
+    private MetricRegistry metricRegistry;
+
     private CreateIndexTask instance;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        this.instance = new CreateIndexTask(repository);
+
+        doReturn(ttlTimer).when(metricRegistry).timer(contains(TTL_INDEX_TIMER_NAME));
+        doReturn(loginTimer).when(metricRegistry).timer(contains(LOGIN_INDEX_TIMER_NAME));
+
+        this.instance = new CreateIndexTask(repository, metricRegistry);
     }
 
     @Test(expected = NullPointerException.class)
     public void constuctorWithNull() throws Exception {
-        new CreateIndexTask(null);
+        new CreateIndexTask(null, metricRegistry);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void constructorWithNullRegistry() throws Exception {
+        new CreateIndexTask(repository, null);
     }
 
     @Test
@@ -34,6 +62,8 @@ public class CreateIndexTaskTest {
         instance.createTtlIndexes();
 
         verify(repository).createTtlIndexes();
+        verify(ttlTimer).update(anyLong(), eq(TimeUnit.MILLISECONDS));
+        verify(loginTimer, never()).update(anyLong(), any());
     }
 
     @Test
@@ -49,6 +79,8 @@ public class CreateIndexTaskTest {
         instance.createUserLoginIndexes();
 
         verify(repository).createUserLoginIndexes();
+        verify(loginTimer).update(anyLong(), eq(TimeUnit.MILLISECONDS));
+        verify(ttlTimer, never()).update(anyLong(), any());
     }
 
     @Test
