e8e085da01806ec399ece444f0b11a96b5e8c886
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2017-08-22 15:01:25 +0200

    TEST: CFAL-193 fix tests for ADS login

--- cfal-test/src/test/java/com/gooddata/cfal/datawarehouse/AdsAutenticationEventsAT.java
+++ cfal-test/src/test/java/com/gooddata/cfal/datawarehouse/AdsAutenticationEventsAT.java
@@ -3,16 +3,26 @@
  */
 package com.gooddata.cfal.datawarehouse;
 
+import com.gooddata.cfal.restapi.dto.AuditEventDTO;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Acceptance test for ADS login and logout
  */
 public class AdsAutenticationEventsAT extends AbstractAdsAT {
 
+    private static final String COMPONENT = "component";
+    private static final String DATAWAREHOUSE = "DATAWAREHOUSE";
+    private static final String USER_NAME_PASSWORD = "USERNAME_PASSWORD";
+    private static final String LOGIN_TYPE = "loginType";
+
     private static final String TEST_QUERY = "SELECT 1";
-    private static final String MESSAGE_TYPE_LOGIN = "DATAWAREHOUSE_USERNAME_PASSWORD_LOGIN";
+    private static final String MESSAGE_TYPE_LOGIN = "LOGIN";
+    private static final String MESSAGE_TYPE_CONNECTION = "CONNECTION";
     private static final String MESSAGE_TYPE_LOGOUT = "DATAWAREHOUSE_LOGOUT";
 
     @BeforeClass(groups = {MESSAGE_TYPE_LOGIN, MESSAGE_TYPE_LOGOUT})
@@ -20,14 +30,24 @@ public class AdsAutenticationEventsAT extends AbstractAdsAT {
         getJdbcTemplate().execute(TEST_QUERY);
     }
 
-    @Test(groups = MESSAGE_TYPE_LOGIN)
+//    @Test(groups = MESSAGE_TYPE_LOGIN) after datawarehouse change passes through RT, uncomment
     public void testUsernamePasswordLoginUserApi() throws InterruptedException {
-        doTestUserApi(pageCheckPredicate(MESSAGE_TYPE_LOGIN), MESSAGE_TYPE_LOGIN);
+        doTestUserApi(loginPageCheckPredicate(), MESSAGE_TYPE_LOGIN);
     }
 
-    @Test(groups = MESSAGE_TYPE_LOGIN)
+//    @Test(groups = MESSAGE_TYPE_LOGIN)
     public void testUsernamePasswordLoginAdminApi() throws InterruptedException {
-        doTestAdminApi(pageCheckPredicate(MESSAGE_TYPE_LOGIN), MESSAGE_TYPE_LOGIN);
+        doTestAdminApi(loginPageCheckPredicate(), MESSAGE_TYPE_LOGIN);
+    }
+
+//    @Test(groups = MESSAGE_TYPE_CONNECTION)
+    public void testADSConnectionUserApi() throws InterruptedException {
+        doTestUserApi(pageCheckPredicate(MESSAGE_TYPE_CONNECTION), MESSAGE_TYPE_CONNECTION);
+    }
+
+//    @Test(groups = MESSAGE_TYPE_CONNECTION)
+    public void testADSConnectionAdminApi() throws InterruptedException {
+        doTestAdminApi(pageCheckPredicate(MESSAGE_TYPE_CONNECTION), MESSAGE_TYPE_CONNECTION);
     }
 
     @Test(groups = MESSAGE_TYPE_LOGOUT)
@@ -38,5 +58,13 @@ public class AdsAutenticationEventsAT extends AbstractAdsAT {
     @Test(groups = MESSAGE_TYPE_LOGOUT)
     public void testDatawarehouseLogoutAdminApi() throws Exception {
         doTestAdminApi(pageCheckPredicate(MESSAGE_TYPE_LOGOUT), MESSAGE_TYPE_LOGOUT);
+    }
+
+    protected Predicate<List<AuditEventDTO>> loginPageCheckPredicate() {
+        return (auditEvents) -> auditEvents.stream().anyMatch(e ->
+                e.getUserLogin().equals(getAccount().getLogin()) &&
+                        e.getType().equals(MESSAGE_TYPE_LOGIN) &&
+                        USER_NAME_PASSWORD.equals(e.getParams().get(LOGIN_TYPE)) &&
+                        DATAWAREHOUSE.equals(e.getParams().get(COMPONENT)));
     }
 }
