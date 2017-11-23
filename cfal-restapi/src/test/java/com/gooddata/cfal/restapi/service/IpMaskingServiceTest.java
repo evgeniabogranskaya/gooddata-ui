7a4f10d263f12725ff1fc5ca44c5a1bf91a84926
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2017-11-23 10:32:35 +0100

    FEATURE: CFAL-236 add ability to pass ip ranges to hide

--- cfal-restapi/src/test/java/com/gooddata/cfal/restapi/service/IpMaskingServiceTest.java
+++ cfal-restapi/src/test/java/com/gooddata/cfal/restapi/service/IpMaskingServiceTest.java
@@ -10,7 +10,6 @@ import static org.hamcrest.Matchers.hasSize;
 import com.gooddata.auditevent.AuditEvent;
 import com.gooddata.auditevent.AuditEvents;
 import org.joda.time.DateTime;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -23,9 +22,9 @@ import java.util.stream.Collectors;
 public class IpMaskingServiceTest {
 
     private static final String IP = "127.0.0.1";
-    private static final String IP_1 = "1";
-    private static final String IP_2 = "2";
-    private static final String IP_3 = "3";
+    private static final String IP_1 = "192.168.1.2";
+    private static final String IP_2 = "192.168.1.5";
+    private static final String IP_3 = "192.169.2.6";
     private static final String ID = "ID";
     private static final String USER = "USER";
     private static final String TYPE = "TYPE";
@@ -33,24 +32,46 @@ public class IpMaskingServiceTest {
     private static final Map<String, String> LINKS = Collections.emptyMap();
     private static final DateTime OCCURRED = new DateTime();
     private static final DateTime RECORDED = new DateTime();
+    private static final String MASK = "/24";
+
+    private final AuditEvent event1 = new AuditEvent(ID, USER, OCCURRED, RECORDED, IP_1, true, TYPE, PARAMS, LINKS);
+    private final AuditEvent event2 = new AuditEvent(ID, USER, OCCURRED, RECORDED, IP_3, true, TYPE, PARAMS, LINKS);
+    private final AuditEvent event3 = new AuditEvent(ID, USER, OCCURRED, RECORDED, IP_2, true, TYPE, PARAMS, LINKS);
+
+    private final AuditEvents events = new AuditEvents(Arrays.asList(event1, event2, event3), null, null);
 
     private IpMaskingService ipMaskingService;
 
-    @Before
-    public void setUp() throws Exception {
-        ipMaskingService = new IpMaskingService(new HashSet<>(Arrays.asList(IP_1, IP_2)));
-    }
 
     @Test
     public void testMasksIps() throws Exception {
-        final AuditEvent event1 = new AuditEvent(ID, USER, OCCURRED, RECORDED, IP_1, true, TYPE, PARAMS, LINKS);
-        final AuditEvent event2 = new AuditEvent(ID, USER, OCCURRED, RECORDED, IP_3, true, TYPE, PARAMS, LINKS);
-        final AuditEvent event3 = new AuditEvent(ID, USER, OCCURRED, RECORDED, IP_2, true, TYPE, PARAMS, LINKS);
+        ipMaskingService = new IpMaskingService(new HashSet<>(Arrays.asList(IP_1, IP_2)));
 
-        final List<AuditEvent> result = ipMaskingService.maskIps(new AuditEvents(Arrays.asList(event1, event2, event3), null, null));
+        final List<AuditEvent> result = ipMaskingService.maskIps(events);
         final List<String> ips = result.stream().map(e -> e.getUserIp()).collect(Collectors.toList());
 
         assertThat(result, hasSize(3));
         assertThat(ips, contains(IP, IP_3, IP));
+    }
+
+    @Test
+    public void testMasksIpsWithinRange() throws Exception {
+        ipMaskingService = new IpMaskingService(new HashSet<>(Arrays.asList(IP_1 + MASK)));
+
+        final List<AuditEvent> result = ipMaskingService.maskIps(events);
+        final List<String> ips = result.stream().map(e -> e.getUserIp()).collect(Collectors.toList());
+
+        assertThat(result, hasSize(3));
+        assertThat(ips, contains(IP, IP_3, IP));
+    }
+    @Test
+    public void testMasksIpsWithinRangeAndExplicitIpAdress() throws Exception {
+        ipMaskingService = new IpMaskingService(new HashSet<>(Arrays.asList(IP_1 + MASK, IP_3)));
+
+        final List<AuditEvent> result = ipMaskingService.maskIps(events);
+        final List<String> ips = result.stream().map(e -> e.getUserIp()).collect(Collectors.toList());
+
+        assertThat(result, hasSize(3));
+        assertThat(ips, contains(IP, IP, IP));
     }
 }
